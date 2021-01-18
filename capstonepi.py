d73e451df4a684d75fe4c3775b2f023e341fa542
Tan Wai Liang <tanwailiang97@gmail.com>
2021-01-18 12:11:08 +0800

    TM1637 1st try

--- capstonepi.py
+++ capstonepi.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep, localtime
 from mlxtemp import updateTemp
 from gpiosetup import gpioInit
-#from tm1637 import TM1637
+from tm1637 import TM1637
 
 
 
@@ -12,26 +12,26 @@ GPIO.add_event_detect(40,GPIO.FALLING,callback = updateTemp)
 DIO = 20
 CLK = 16
 
-# class Clock:
-#     def __init__(self, tm_instance):
-#         self.tm = tm_instance
-#         self.show_colon = False
+class Clock:
+    def __init__(self, tm_instance):
+        self.tm = tm_instance
+        self.show_colon = False
 
-#     def run(self):
-#         while True:
-#             t = localtime()
-#             self.show_colon = not self.show_colon
-#             tm.numbers(t.tm_hour, t.tm_min, self.show_colon)
-#             sleep(1)
+    def run(self):
+        while True:
+            t = localtime()
+            self.show_colon = not self.show_colon
+            tm.numbers(t.tm_hour, t.tm_min, self.show_colon)
+            sleep(1)
 
 
-# if __name__ == '__main__':
-#     tm = TM1637(CLK, DIO)
-#     tm.brightness(1)
+if __name__ == '__main__':
+    tm = TM1637(CLK, DIO)
+    tm.brightness(1)
 
-#     clock = Clock(tm)
-#     clock.run()
+    clock = Clock(tm)
+    clock.run()
 
-while True:
-    sleep(0.5)
+# while True:
+#     sleep(0.5)
 
--- gpiosetup.py
+++ gpiosetup.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 def gpioInit():
     GPIO.setwarnings(False)
-    # GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(26,GPIO.OUT,initial=GPIO.LOW)
     # GPIO.setup(36,GPIO.OUT,initial=GPIO.LOW)
     # GPIO.setup(38,GPIO.OUT,initial=GPIO.LOW)

38a5bf1136cf2737edee04fbcb6262e2c5acf4fe
Tan Wai Liang <tanwailiang97@gmail.com>
2021-01-18 13:57:44 +0800

    Removing colon

--- gpiosetup.py
+++ gpiosetup.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 def gpioInit():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(26,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(37,GPIO.OUT,initial=GPIO.LOW)
     # GPIO.setup(36,GPIO.OUT,initial=GPIO.LOW)
     # GPIO.setup(38,GPIO.OUT,initial=GPIO.LOW)
-    GPIO.setup(21,GPIO.IN)
+    GPIO.setup(40,GPIO.IN)
